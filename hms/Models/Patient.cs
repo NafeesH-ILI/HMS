788bdb0e02b2b7f5e7a019d6afb567d8318ea64a
nafees <nafees.hassan@ili.digital>
2025-08-12 16:02:56 +0500

    Added Patiient model

---
+++ hms/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace hms.Models
+{
+    [Table("patients")]
+    [PrimaryKey(nameof(Phone), nameof(Name))]
+    public class Patient
+    {
+        [Required]
+        [Column("phone")]
+        [Length(12, 12)]
+        public required string Phone { get; set; }
+
+        [Required]
+        [Column("name")]
+        public required string Name { get; set; }
+
+        [Required]
+        [Column("dob")]
+        public DateOnly DateBirth { get; set; }
+    }
+}
