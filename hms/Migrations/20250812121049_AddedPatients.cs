8eb1941937c37143014b0d9834167fdb5df9ad06
nafees <nafees.hassan@ili.digital>
2025-08-12 17:11:23 +0500

    migrations for adding patient

---
+++ hms/Migrations/20250812121049_AddedPatients.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hms.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPatients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "patients",
+                columns: table => new
+                {
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    dob = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_patients", x => new { x.phone, x.name });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "patients");
+        }
+    }
+}
