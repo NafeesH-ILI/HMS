ff032df5c0a2b63f4b438f5ce8b44c64d8a10d40
nafees <nafees.hassan@ili.digital>
2025-08-12 14:10:21 +0500

    Added error types

--- hms/Errors.cs
+++ hms/Errors.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net.Http.Headers;
 
 namespace hms
 {
@@ -9,7 +10,18 @@ namespace hms
         //private readonly ILogger<ErrorHandlerAttribute> _logger = logger;
         public override void OnException(ExceptionContext ctx)
         {
-            ctx.Result = new BadRequestResult();
+            if (ctx.Exception is ErrNotFound)
+            {
+                ctx.Result = new NotFoundResult();
+            }
+            else if (ctx.Exception is ErrBadReq)
+            {
+                ctx.Result = new BadRequestResult();
+            }
         }
     }
+
+    public class ErrNotFound() : Exception { }
+
+    public class ErrBadReq() : Exception { }
 }
