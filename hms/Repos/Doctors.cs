daebb5d69b08b8bf6dd967dd491f7c61d4a012fb
nafees <nafees.hassan@ili.digital>
2025-08-12 12:26:11 +0500

    Doctors Repo

---
+++ hms/Repos/Doctors.cs
@@ -0,0 +1,62 @@
+﻿using hms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hms.Repos
+{
+    public class Doctors (DbCtx ctx)
+    {
+        private readonly DbCtx _ctx = ctx;
+
+        public async Task<int> Count(string? fmt = null)
+        {
+            if (fmt == null)
+            {
+                return await _ctx.Doctors.CountAsync();
+            }
+            return await _ctx.Doctors
+            .Where(d => d.Name != null && d.Name.Contains(fmt))
+            .CountAsync();
+        }
+
+        public async Task<Doctor?> GetByUName(string uname)
+        {
+            return await _ctx.Doctors.FindAsync(uname);
+        }
+
+        public async Task<bool> ExistsByUName(string uname)
+        {
+            return await _ctx.Doctors
+                .Where(d => d.UName == uname)
+                .CountAsync() > 0;
+        }
+
+        public async Task<IList<Doctor>> Get(string uname, int page = 1, int pageSize = 10)
+        {
+            if (page < 1 || pageSize <= 0)
+                throw new Exception("Invalid pagination");
+            return await ctx.Doctors
+                 .OrderBy(d => d.UName)
+                 .Skip((page - 1) * pageSize)
+                 .Take(pageSize)
+                 .ToListAsync();
+        }
+
+        public async void Add(Doctor doctor)
+        {
+            _ctx.Doctors.Add(doctor);
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async void Update(Doctor doctor)
+        {
+            _ctx.Doctors.Update(doctor);
+            await _ctx.SaveChangesAsync();
+        }
+        
+        public async void Delete(string uname)
+        {
+            _ctx.Doctors.Remove((await GetByUName(uname))!);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
