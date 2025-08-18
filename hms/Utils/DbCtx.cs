e101edccc0801f12783d9d7f2f41ae1718d6eb95
Nafees Hassan <nafeeshassan16@gmail.com>
2025-08-18 23:51:15 +0500

    it works

--- hms/Utils/DbCtx.cs
+++ hms/Utils/DbCtx.cs
@@ -16,12 +16,37 @@ namespace hms.Common
         public DbCtx() { }
         public DbCtx(DbContextOptions<DbCtx> options) : base(options) { }
 
-        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<User>().HasData(
-                new User { Type = User.Types.SuperAdmin, PassHash = "abcd", UName = "sudo"});
-        }*/
+            /*modelBuilder.Entity<User>().HasData(
+                new User { Type = User.Types.SuperAdmin, PassHash = "abcd", UName = "sudo"});*/
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = "1",
+                UserName = "sudo",
+                NormalizedUserName = "SUDO",
+                Email = "sudo@example.com",
+                NormalizedEmail = "SUDO@EXAMPLE.COM",
+                EmailConfirmed = true,
+                Type = User.Types.SuperAdmin,
+                SecurityStamp = "f613a119-e134-428c-8dec-98bf5c82ea2e",
+                ConcurrencyStamp = "f613a119-e134-428c-8dec-98bf5c82ea2a",
+                PasswordHash = "AQAAAAIAAYagAAAAEOVIKgI78Fd2jA/QBnJM/7uFMuPgl2jrht/8Z7hVY7kJU37/hOW3V6PIP2AihhitBg=="
+            });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "64a1c0ab-8783-4cb7-ad7d-254c050815aa",
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN"
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                UserId = "1",
+                RoleId = "64a1c0ab-8783-4cb7-ad7d-254c050815aa"
+            });
+        }
 
         public bool CanConnect()
         {
